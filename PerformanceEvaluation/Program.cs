9bd0f433b26c113785aa4d68af69e91d28403b3a
Smolyaninala <Smolyaninala@arkona36.ru>
2021-05-24 21:54:02 +0300

    Добавьте файлы проекта.

---
+++ PerformanceEvaluation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace PerformanceEvaluation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> textList = new List<string>();
+
+            //LinkedList<string> testLinked = new LinkedList<string>();
+
+            string pathText = @"C:\TaskDir\Text1.txt";
+
+            char[] delimiterChars = { ' ', ',', '.', ':', '\r', '\n', '«', '»','?','(',')','!',';','-' };
+
+            using (StreamReader sr = new StreamReader(pathText))
+            {
+                var text = sr.ReadToEnd();
+                var words = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+
+                var watchList = Stopwatch.StartNew();
+                textList.AddRange(words);
+                watchList.Stop();
+
+                var wathLinkedlist = Stopwatch.StartNew();
+                LinkedList<string> testLinked = new LinkedList<string>(words);
+                wathLinkedlist.Stop();
+
+               
+                Console.WriteLine("время производительности List: {0}  LinkedList: {1}", watchList.Elapsed.TotalMilliseconds,wathLinkedlist.Elapsed.TotalMilliseconds);
+
+               
+                sr.Close();
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
